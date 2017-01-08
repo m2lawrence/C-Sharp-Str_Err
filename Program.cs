0621b5ea17e26c54248fddc3756ba2a14c39719c
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2017-01-08 22:24:49 +0000

    Add files via upload
    // 11.7 Strings Cannot Be Changed.
    // If you really need to modify a string, C# provides another class that can be used—the StringBuilder class.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// 11.7 Strings Cannot Be Changed.
+// If you really need to modify a string, C# provides another class that can be used—the StringBuilder class.
+using System;
+namespace Str_Err
+{
+	class Str_Err
+	{
+		public static void Main(string[] args)
+		{
+			string str1 = "mike";
+			str1[2] = 'I'; //Error!!! Tries to change i to I, says it's "read only" as can't be changed.
+			Console.WriteLine(str1);
+			Console.ReadKey(true);
+		}
+	}
+}
